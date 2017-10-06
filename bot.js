5322fd5996df13e541c9a025d60db2e5084860f5
Gianni <gianni.infi@gmail.com>
2017-10-06 16:29:37 -0400

    Change Node Dependencies to my machine

--- bot.js
+++ bot.js
@@ -17,59 +17,23 @@ bot.on('ready', function (evt) {
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
-var math = false
-var math_input_first
-var math_input_second
-var count
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!' && isNaN(message)) {
+    if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
-        var cmd2;
+       
         args = args.splice(1);
-
-        if(math == false)
-        {
         switch(cmd) {
-
+            // !ping
             case 'ping':
                 bot.sendMessage({
                     to: channelID,
                     message: 'Pong!'
                 });
             break;
-
-            case 'test':
-                math = true
-            break;
+            // Just add any case commands if you want to..
          }
-       }
-       else if(count == 0)
-       {
-         math_input_first = cmd
-         count = 1
-       }
-       else
-       {
-         math_input_second = cmd
-         test_func(this, channelID)
-       }
      }
 });
-
-function test_func(obj, channelID)
-{
-  console.log('test')
-  var term1 = parseInt(math_input_first)
-  var term2 = parseInt(math_input_second)
-
-  obj.sendMessage({
-    to: channelID,
-    message: term1 + term2
-  })
-  count = 0
-  math = false
-
-}
