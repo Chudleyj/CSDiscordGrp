a98b97f0759db0990879542b0a36817cf82e8ba6
Justin Chudley <chudleyjustin@gmail.com>
2017-10-06 17:20:10 -0400

    Streamlined and improved sum function.

--- bot.js
+++ bot.js
@@ -12,26 +12,27 @@ var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
-bot.on('ready', function (evt) {
+bot.on('ready', function (evt, channelID) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
+    bot.sendMessage({
+      to: channelID,
+      message: 'Oh shit here come dat boi!'
+    })
 });
-var math = false
-var math_input_first
-var math_input_second
-var count
+
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!' && isNaN(message)) {
+
+    if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
-        var cmd2;
-        args = args.splice(1);
+        var cmd2 = args[1];
+        var cmd3 = args[2];
+       args = args.splice(1);
 
-        if(math == false)
-        {
         switch(cmd) {
 
             case 'ping':
@@ -41,35 +42,21 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 });
             break;
 
-            case 'test':
-                math = true
+            case 'sum':
+                test_func(bot, channelID, cmd2, cmd3)
             break;
          }
-       }
-       else if(count == 0)
-       {
-         math_input_first = cmd
-         count = 1
-       }
-       else
-       {
-         math_input_second = cmd
-         test_func(this, channelID)
-       }
      }
 });
 
-function test_func(obj, channelID)
+function test_func(obj, channelID, input1, input2)
 {
   console.log('test')
-  var term1 = parseInt(math_input_first)
-  var term2 = parseInt(math_input_second)
+  var sum = parseInt(input1) + parseInt(input2)
 
   obj.sendMessage({
     to: channelID,
-    message: term1 + term2
+    message: sum
   })
-  count = 0
-  math = false
 
 }
