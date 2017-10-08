7894e72303247d2619caa38ab6a38878f2d8d924
Justin Chudley <chudleyjustin@gmail.com>
2017-10-08 19:08:40 -0400

    This bot has been changed into a Trivia bot. Only has a few questions so far.

--- bot.js
+++ bot.js
@@ -16,47 +16,188 @@ bot.on('ready', function (evt, channelID) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
-    bot.sendMessage({
-      to: channelID,
-      message: 'Oh shit here come dat boi!'
-    })
 });
 
+var on = false
+var question_choice
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
 
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
-        var cmd2 = args[1];
-        var cmd3 = args[2];
+        var ans = message.substring(1);
        args = args.splice(1);
 
-        switch(cmd) {
+       if(on == false){
+       switch(cmd){
+          case 'start':
+            get_Question(channelID);
+          break;
+        }
+      }
 
-            case 'ping':
+      else if(question_choice == 1){
+        switch(ans){
+            case 'Pony Express':
+            case 'Pony express':
+            case 'pony express':
+            case 'pony Express':
+            bot.sendMessage({
+              to: channelID,
+              message: "Correct!"
+            })
+            on = false;
+            break;
+
+
+            default:
+            bot.sendMessage({
+              to: channelID,
+              message: "Wrong!"
+            })
+            on = false;
+            break;
+          }
+        }
+
+        else if(question_choice == 2){
+          switch(cmd){
+              case 'Camel':
+              case 'camel':
+              bot.sendMessage({
+                to: channelID,
+                message: "Correct!"
+              })
+              on = false;
+              break;
+
+              default:
+              bot.sendMessage({
+                to: channelID,
+                message: "Wrong!"
+              })
+              on = false;
+              break;
+            }
+          }
+
+          else if(question_choice == 3){
+            switch(cmd){
+                case 'Vincent Van Gogh':
+                case 'Vincent Van gogh':
+                case 'Vincent van gogh':
+                case 'Vincent van Gogh':
+                case 'vincent van gogh':
                 bot.sendMessage({
+                  to: channelID,
+                  message: "Correct!"
+                })
+                on = false;
+                break;
+
+                default:
+                bot.sendMessage({
+                  to: channelID,
+                  message: "Wrong!"
+                })
+                on = false;
+                break;
+              }
+            }
+
+            else if(question_choice == 4){
+              switch(cmd){
+                  case '1990':
+                  bot.sendMessage({
                     to: channelID,
-                    message: 'Pong!'
-                });
-            break;
+                    message: "Correct!"
+                  })
+                  on = false;
+                  break;
 
-            case 'sum':
-                test_func(bot, channelID, cmd2, cmd3)
-            break;
-         }
-     }
-});
+                  default:
+                  bot.sendMessage({
+                    to: channelID,
+                    message: "Wrong!"
+                  })
+                  on = false;
+                  break;
+                }
+              }
 
-function test_func(obj, channelID, input1, input2)
-{
-  console.log('test')
-  var sum = parseInt(input1) + parseInt(input2)
+              else if(question_choice == 5){
+                switch(cmd){
+                    case 'Liquid Crystal Display':
+                    case 'Liquid crystal display':
+                    case 'Liquid Crystal display':
+                    case 'Liquid crystal Display':
+                    case 'liquid Crystal Display':
+                    case 'liquid crystal Display':
+                    case 'liquid crystal display':
+                    bot.sendMessage({
+                      to: channelID,
+                      message: "Correct!"
+                    })
+                    on = false;
+                    break;
 
-  obj.sendMessage({
-    to: channelID,
-    message: sum
-  })
+                    default:
+                    bot.sendMessage({
+                      to: channelID,
+                      message: "Wrong!"
+                    })
+                    on = false;
+                    break;
+                  }
+                }
+    }//end if (message....)
 
+});// end function
+
+function get_Question(channelID){
+  question_choice = getRandomInt(1,5)
+  switch(question_choice){
+    case 1:
+      bot.sendMessage({
+        to: channelID,
+        message: 'What was the name of the U.S. mail service, started in 1860, that used horses and riders?'
+      });
+    break;
+
+    case 2:
+      bot.sendMessage({
+        to: channelID,
+        message: 'What type of animal is known as the ship of the desert?'
+      });
+    break;
+
+    case 3:
+      bot.sendMessage({
+        to: channelID,
+        message: 'The Starry Night is an oil on canvas painted by which post-impressionist painter?'
+      });
+    break;
+
+    case 4:
+      bot.sendMessage({
+        to: channelID,
+        message: 'CERN launched the very first website in what year?'
+      });
+    break;
+
+    case 5:
+      bot.sendMessage({
+        to: channelID,
+        message: 'When referring to a computer monitor, what does the acronym LCD stand for?'
+      });
+    break;
+  }
+  on = true
+  return question_choice;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 }
