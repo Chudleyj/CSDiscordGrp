66dd2140a2f317bd714ebfd520f2d85774169496
Justin Chudley <chudleyjustin@gmail.com>
2017-10-08 20:00:22 -0400

    More questions

--- bot.js
+++ bot.js
@@ -22,12 +22,17 @@ var on = false
 var question_choice
 bot.on('message', function (user, userID, channelID, message, evt) {
 
-    if (message.substring(0, 1) == '!') {
+    if (message.substring(0, 1) == ';') {
         var ans = message.substring(1); //Question answer vars
         var args = message.substring(1).split(' ');
         var cmd = args[0];
         ans = ans.toLowerCase();
-       args = args.splice(1);
+        args = args.splice(1);
+
+        bot.sendMessage({
+          to: channelID,
+          message: ans
+        })
 
        if(on == false){
        switch(cmd){
@@ -137,12 +142,72 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     break;
                   }
                 }
+
+                else if(question_choice == 6){
+                  switch(cmd){
+                      case 'read only memory':
+                      bot.sendMessage({
+                        to: channelID,
+                        message: "Correct!"
+                      })
+                      on = false;
+                      break;
+
+                      default:
+                      bot.sendMessage({
+                        to: channelID,
+                        message: "Wrong!"
+                      })
+                      on = false;
+                      break;
+                    }
+                  }
+
+                  else if(question_choice == 7){
+                    switch(cmd){
+                        case 'kodak':
+                        bot.sendMessage({
+                          to: channelID,
+                          message: "Correct!"
+                        })
+                        on = false;
+                        break;
+
+                        default:
+                        bot.sendMessage({
+                          to: channelID,
+                          message: "Wrong!"
+                        })
+                        on = false;
+                        break;
+                      }
+                    }
+
+                    else if(question_choice == 8){
+                      switch(cmd){
+                          case '1993':
+                          bot.sendMessage({
+                            to: channelID,
+                            message: "Correct!"
+                          })
+                          on = false;
+                          break;
+
+                          default:
+                          bot.sendMessage({
+                            to: channelID,
+                            message: "Wrong!"
+                          })
+                          on = false;
+                          break;
+                        }
+                      }
     }//end if (message....)
 
 });// end function
 
 function get_Question(channelID){
-  question_choice = getRandomInt(1,5)
+  question_choice = getRandomInt(1,9)
   switch(question_choice){
     case 1:
       bot.sendMessage({
@@ -180,10 +245,24 @@ function get_Question(channelID){
     break;
 
 
-  case 5:
+  case 6:
     bot.sendMessage({
       to: channelID,
       message: 'When talking about computer memory, what does the acronym ROM stand for?'
+    });
+  break;
+
+  case 7:
+    bot.sendMessage({
+      to: channelID,
+      message: 'In 1975 an engineer created the first electronic camera while working for what company?'
+    });
+  break;
+
+  case 8:
+    bot.sendMessage({
+      to: channelID,
+      message: 'The first person shooter video game Doom was first released in what year?'
     });
   break;
 }
